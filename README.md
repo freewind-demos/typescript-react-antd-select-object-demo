ce53a1ef0aa9ea81bbda38a69e7ae6bed7112d10
Freewind <nowindlee@gmail.com>
2022-04-18 20:03:09 +0800

    typescript-react-antd-select-object-demo

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-TypeScript React AntDesign Select Demo
+TypeScript React AntDesign Select Object Demo
 =================================
 
-AntD中的Select有一个严重的问题，就是option中选项如果过长，后面的内容会被隐藏，在有些情况下无法使用。
-
-这里通过css对其进行override时发现一个问题：
-option选项会被直接插入到body下，而不是我们自己定义的组件下。为了能对它进行css覆盖，需要给定一个dropdownClassName名字，再对其覆写。
+AntD中的Select不支持传入一个object[]，所以只能通过value (number/string)反查，有点麻烦。
 
 ```
 npm install
--- src/MySelect.tsx
+++ src/MySelect.tsx
@@ -1,11 +1,27 @@
 import {Select} from 'antd';
-import React from 'react'
+import React, {useState} from 'react'
 
 import './MySelect.css';
 
+type User = {
+  id: number,
+  name: string,
+  email: string
+}
+
+const users: User[] = [
+  {id: 1, name: "Jeff", email: "jeff@test.com"},
+  {id: 2, name: "Mike", email: "mike@test.com"},
+  {id: 3, name: "Tom", email: "tom@test.com"},
+]
 export default function MySelect() {
-  const users = ['aaa', 'bbb', 'ccc cc cccc cc cccc', 'ddd']
-  return <Select className='hello' dropdownClassName={'wrapped-select-option'} placeholder={'User'}>
-    {users.map(user => <Select.Option key={user} value={user}>{user}</Select.Option>)}
-  </Select>
+  const [selectedUser, setSelectedUser] = useState<User>();
+
+  const options = users.map(it => ({label: it.name, value: it.id}));
+
+  return <Select placeholder={'User'}
+                 options={options}
+                 value={selectedUser?.id}
+                 onChange={value => setSelectedUser(users.find(it => it.id === value))}
+  />
 };
